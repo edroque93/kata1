ec564764efd7b92687205dd4182c523e0c173af2
Quique <Quique@10.230.169.43>
2013-10-06 11:55:56 +0100

    Mi ejemplo.

---
+++ src/kata1/Item.java
@@ -0,0 +1,14 @@
+package kata1;
+
+public class Item {
+
+    private int value;
+
+    public Item(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
---
+++ src/kata1/Kata1.java
@@ -0,0 +1,9 @@
+package kata1;
+
+public class Kata1 {
+
+    public static void main(String[] args) {
+        Item item = new Item(3);
+    }
+
+}
